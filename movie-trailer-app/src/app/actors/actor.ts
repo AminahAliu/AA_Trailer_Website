e0d49f78d290124308d6d08f9bc87bfde695979c
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-24 10:35:52 -0800

    feat: give each actor an id #

--- movie-trailer-app/src/app/actors/actor.ts
+++ movie-trailer-app/src/app/actors/actor.ts
@@ -4,4 +4,5 @@ export class Actor {
   name: string;
   movies: Array<Movie>;
   image: string;
+  id: integer; 
 }
--- movie-trailer-app/src/app/actors/real-actors.ts
+++ movie-trailer-app/src/app/actors/real-actors.ts
@@ -9,17 +9,20 @@ import { THE_COLOR_PURPLE } from './real-movies';
 import { THE_BUTLER } from './real-movies';
 
 export const MRS_WHICH: Actor = {
+  id: 1,
   name: 'Oprah Winfrey',
   movies: [A_WRINKLE_IN_TIME, THE_COLOR_PURPLE, THE_BUTLER],
   image: 'assets/img/OprahWinfrey.jpg',
 };
 export const PRINCE_TCHALLA: Actor = {
+  id: 2,
   name: 'Chadwick Boseman',
   movies: [BLACK_PANTHER, MARSHALL, GET_ON_UP],
   image: 'assets/img/ChadwickBoseman.jpg'
 };
 
 export const MEG_MURRY: Actor = {
+  id: 3,
   name: 'Storm Reid',
   movies: [A_WRINKLE_IN_TIME, SLEIGHT, A_HAPPENING_OF_MONUMENTAL_PROPORTIONS],
   image: 'assets/img/StormReid.jpg'
