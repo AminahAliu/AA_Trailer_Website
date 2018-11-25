965c2a4455a4e0a39a15575ee4667547be220956
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-25 10:05:58 -0800

    feat: adjust routes variable in app-routing.module.ts

--- movie-trailer-app/src/app/app-routing.module.ts
+++ movie-trailer-app/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import { ActorsComponent } from './actors/actors.component';
+import { ActorDetailComponent } from './actor-detail/actor-detail.component';
 
 const routes: Routes = [
-  {path: '', component: ActorsComponent},
+  { path: '', component: ActorsComponent },
+  { path: 'detail/:id', component: ActorDetailComponent }
 ];
 
 @NgModule({
