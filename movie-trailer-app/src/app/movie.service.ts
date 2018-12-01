2f90852b33a67f14a5d4dd2a3b76b6928ca0c5e8
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-30 21:32:40 -0800

    fix: edit dates for functions

--- movie-trailer-app/src/app/movie.service.ts
+++ movie-trailer-app/src/app/movie.service.ts
@@ -18,7 +18,7 @@ export class MovieService {
 
 getUpcomingMovies() {
     // tslint:disable-next-line:max-line-length
-    return this._http.get(this.movie_url + 'discover/movie?primary_release_date.gte=2018-04-15&primary_release_date.lte=2018-07-31' + '&api_key=' + this.api_key);
+    return this._http.get(this.movie_url + 'discover/movie?primary_release_date.gte=2018-11-25&primary_release_date.lte=2019-02-28' + '&api_key=' + this.api_key);
   }
 
   getPopularMovies() {
@@ -30,3 +30,16 @@ getUpcomingMovies() {
   }
 
 }
+
+// Get popular movies api
+// https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=API_KEY
+
+// Get upcoming movies api
+// tslint:disable-next-line:max-line-length
+// https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2018-11-25&primary_release_date.lte=2019-02-28&api_key=API_KEY
+
+// Search movies api
+// https://api.themoviedb.org/3/search/movie?query=SEARCH_STRING&api_key=API_KEY
+
+// Get movie from api
+// https://api.themoviedb.org/3/movie/ID?api_key=API_KEY
