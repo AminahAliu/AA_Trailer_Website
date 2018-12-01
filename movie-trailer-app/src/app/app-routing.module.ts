1930439bad665d68b1f8a39b68fa9d6f51a82993
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-30 20:34:29 -0800

    feat: add app routes for movie component

--- movie-trailer-app/src/app/app-routing.module.ts
+++ movie-trailer-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ActorsComponent } from './actors/actors.component';
 import { ActorDetailComponent } from './actor-detail/actor-detail.component';
+import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
   { path: '', component: ActorsComponent },
--- movie-trailer-app/src/app/app.module.ts
+++ movie-trailer-app/src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { ActorsComponent } from './actors/actors.component';
 import { ActorDetailComponent } from './actor-detail/actor-detail.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from './movie.service';
+import { MovieComponent } from './movie/movie.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: ActorsComponent },
+  { path: 'movie/:movieID', component: MovieComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ActorsComponent,
-    ActorDetailComponent
+    ActorDetailComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
     ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
