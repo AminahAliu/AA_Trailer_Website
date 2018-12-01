48ea9f675c69c55493aaff0b3ab9147696c078a1
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-30 21:16:21 -0800

    feat:subscribe to all the methods in the MovieService to fetch the data.

--- movie-trailer-app/src/app/movie-home/movie-home.component.ts
+++ movie-trailer-app/src/app/movie-home/movie-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-home',
@@ -6,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-home.component.css']
 })
 export class MovieHomeComponent implements OnInit {
+  popular_movies: any;
+  upcoming_movies: any;
+  search_result: any;
+  movie: any;
 
-  constructor() { }
+  constructor(public movieService: MovieService) {
+    // get upcoming movies
+    this.movieService.getUpcomingMovies().subscribe(data => {
+      this.upcoming_movies = data['results'];
+      // console.log(this.upcoming_movies);
+    });
+
+    // get popular movies
+    this.movieService.getPopularMovies().subscribe(data => {
+      this.popular_movies = data['results'];
+      // console.log(this.popular_movies);
+    });
+
+  }
+
+  // get search results of movies
+  searchMovies() {
+    this.movieService.searchMovie(this.movie).subscribe(data => {
+      this.search_result = data['results'];
+      // console.log(this.search_result);
+    });
+  }
 
   ngOnInit() {
   }
