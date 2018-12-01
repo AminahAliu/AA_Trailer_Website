90a2ca0c2ffa23a94b119e4e092d0bf6007891db
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-11-30 21:09:18 -0800

    feat: add functions to get movie data style: change text in actor's component

--- movie-trailer-app/src/app/movie.service.ts
+++ movie-trailer-app/src/app/movie.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class MovieService {
 
-  constructor() { }
+  private movie_url = 'https://api.themoviedb.org/3/';
+  private api_key = 'ed6231f16be794a6e311f65e095f0c13';
+  private movie_string: string;
+  private id: number;
+
+  constructor(public _http: HttpClient) { }
+
+  searchMovie(movie: string) {
+  this.movie_string = movie;
+  return this._http.get(this.movie_url + 'search/movie?query=' + this.movie_string + '&api_key=' + this.api_key);
+}
+
+getUpcomingMovies() {
+    // tslint:disable-next-line:max-line-length
+    return this._http.get(this.movie_url + 'discover/movie?primary_release_date.gte=2018-04-15&primary_release_date.lte=2018-07-31' + '&api_key=' + this.api_key);
+  }
+
+  getPopularMovies() {
+    return this._http.get(this.movie_url + 'discover/movie?sort_by=popularity.desc' + '&api_key=' + this.api_key);
+  }
+
+  getMovie(id: number) {
+    return this._http.get(this.movie_url + 'movie/' + id + '?api_key=' + this.api_key);
+  }
+
 }
