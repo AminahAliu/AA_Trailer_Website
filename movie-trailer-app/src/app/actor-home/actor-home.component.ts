ba9e1c9c43120eced5066efecae3adbc11fbb6c2
Aminah Aliu <aminahaliu@Aminahs-MacBook-Air.local>
2018-12-05 13:16:13 -0800

    feat: add content to actor-home html and ts page

--- movie-trailer-app/src/app/actor-home/actor-home.component.ts
+++ movie-trailer-app/src/app/actor-home/actor-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-actor-home',
@@ -6,8 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actor-home.component.css']
 })
 export class ActorHomeComponent implements OnInit {
+  popular_movies: any;
+  upcoming_movies: any;
+  search_result: any;
+  movie: any;
+  popular_people: any;
 
-  constructor() { }
+  constructor(public movieService: MovieService) {
+    // get upcoming movies
+    this.movieService.getUpcomingMovies().subscribe(data => {
+      this.upcoming_movies = data['results'];
+      // console.log(this.upcoming_movies);
+    });
+
+    // get popular movies
+    this.movieService.getPopularMovies().subscribe(data => {
+      this.popular_movies = data['results'];
+      // console.log(this.popular_movies);
+    });
+
+    // get popular people
+    this.movieService.getPopularPeople().subscribe(data => {
+      this.popular_people = data['results'];
+      // console.log(this.popular_people);
+    });
+  }
+
+  // get search results of movies
+  searchMovies() {
+    this.movieService.searchMovie(this.movie).subscribe(data => {
+      this.search_result = data['results'];
+      // console.log(this.search_result);
+    });
+  }
 
   ngOnInit() {
   }
