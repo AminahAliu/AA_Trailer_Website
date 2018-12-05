b2e040b608d543f0f50eff36eddeb43f56d2f95c
Aminah Aliu <aminahaliu@Aminahs-MacBook-Air.local>
2018-12-05 13:25:14 -0800

    feat: create actor-api component

---
+++ movie-trailer-app/src/app/actor-api/actor-api.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActorApiComponent } from './actor-api.component';
+
+describe('ActorApiComponent', () => {
+  let component: ActorApiComponent;
+  let fixture: ComponentFixture<ActorApiComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActorApiComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActorApiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ movie-trailer-app/src/app/actor-api/actor-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-api',
+  templateUrl: './actor-api.component.html',
+  styleUrls: ['./actor-api.component.css']
+})
+export class ActorApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
