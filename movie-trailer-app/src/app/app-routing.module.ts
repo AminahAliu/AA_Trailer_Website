931c850ced22b427ea490a51cc1cb14699efbc53
Aminah Aliu <aminahaliu@Aminahs-Air.T-mobile.com>
2018-12-07 20:08:27 -0800

    fix: corrected routing to ActorApiComponent and deleted everything from actor-api.component.html (will recode later to fix another bug in display)

--- movie-trailer-app/src/app/app-routing.module.ts
+++ movie-trailer-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-
+import { ActorApiComponent } from './actor-api/actor-api.component';
 import { ActorsComponent } from './actors/actors.component';
 import { ActorDetailComponent } from './actor-detail/actor-detail.component';
 import { MovieComponent } from './movie/movie.component';
@@ -10,7 +10,7 @@ import { MovieComponent } from './movie/movie.component';
 const routes: Routes = [
   { path: '', component: ActorsComponent },
   { path: 'detail/:id', component: ActorDetailComponent },
-  { path: 'person/:id', component: ActorDetailComponent }
+  { path: 'person/:id', component: ActorApiComponent }
 ];
 
 @NgModule({
